fd45977d854bf435fda54990a5a1353449aa300a
ferygood <yaochung41@gmail.com>
2021-03-06 13:59:09 +0100

    fix h3

--- assignment_w2/hw3.js
+++ assignment_w2/hw3.js
@@ -1,10 +1,14 @@
 function maxProduct(nums){
-    var sort_nums = nums.sort(function(a,b){
-        return b-a; // b-a > 0 反向排序
-    });
-    product = sort_nums[0] * sort_nums[1];
-    return product;
+    var product_list = [];
+    for (n=0; n<nums.length-1; n++){
+        for (m=n+1; m<nums.length; m++){
+            var product = nums[n]*nums[m];
+            product_list.push(product);
+        }
+    }
+    return Math.max.apply(Math,product_list);
 }
 
 console.log(maxProduct([5,20,2,6])); //120
 console.log(maxProduct([10,-20,0,3])); //30
+console.log(maxProduct([-10,-20,1,2])); //200
--- assignment_w2/hw3.py
+++ assignment_w2/hw3.py
@@ -1,10 +1,11 @@
 def maxProduct(nums):
-    # 從大到小排序
-    sort_nums = sorted(nums, reverse=True)
-    # 取最大的兩位相乘
-    product = sort_nums[0]*sort_nums[1]
-    return product
+    product_list = []
+    for n in range(len(nums)-1):
+        for m in range(n+1,len(nums)):
+            product = nums[n]*nums[m]
+            product_list.append(product)
+    return max(product_list)
 
 print(maxProduct([5,20,2,6])) #120
 print(maxProduct([10,-20,0,3])) #30
-
+print(maxProduct([-10,-20,1,2])) #200
